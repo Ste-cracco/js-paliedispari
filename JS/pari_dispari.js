81b6cb682ba9ed4f548db96c5f0e31cad64970cf
Ste-cracco <s.cracco@yahoo.it>
2022-07-28 16:29:16 +0200

    problema function numero pari o dispari

--- JS/pari_dispari.js
+++ JS/pari_dispari.js
@@ -1,19 +1,30 @@
-let sceltaUtente = prompt("Scegli tra: 'pari' o 'dispari'" );
+// let sceltaUtente = prompt("Scegli tra: 'pari' o 'dispari'" );
 
-sceltaUtente = true;
+// sceltaUtente = true;
 
-if (sceltaUtente !== "pari" || sceltaUtente !== "dispari") {
-    sceltaUtente = false;
-    console.log ("Devi inserire solo la parola 'pari' o 'dispari' ")
+// if (sceltaUtente !== "pari" || sceltaUtente !== "dispari") {
+//     sceltaUtente = false;
+//     console.log ("Devi inserire solo la parola 'pari' o 'dispari' ")
+// }
+
+// const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+// function generazioneNumeroRandom () {
+//     const numeroRandom = parseInt(Math.floor(Math.random() * 6));
+//     return numeroRandom;
+// }
+
+// const numeroComputer = generazioneNumeroRandom();
+// const sommaNumeri = numeroUtente + numeroComputer;
+
+function numeroPariDispari(numero) {
+    numero = false;
+    if(numero % 2 === 0) {
+        numero = true;
+    }
+    return numero;
 }
 
-const numeroUtente = prompt("Inserisci un numero da 1 a 5");
-
-function generazioneNumeroRandom () {
-    const numeroRandom = Math.floor(Math.random() * 6);
-    return numeroRandom;
-}
-
-const numeroComputer = generazioneNumeroRandom();
-
+let ciao = numeroPariDispari(3)
+console.log(ciao)
 
