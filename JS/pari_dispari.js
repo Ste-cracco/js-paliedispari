05f27a5750b9f198bae9898a53a7e638407fbe98
Ste-cracco <s.cracco@yahoo.it>
2022-07-28 17:20:24 +0200

    Problema condizione if finale

--- JS/pari_dispari.js
+++ JS/pari_dispari.js
@@ -1,30 +1,42 @@
-// let sceltaUtente = prompt("Scegli tra: 'pari' o 'dispari'" );
+let sceltaUtente = prompt("Scegli tra: 'pari' o 'dispari'" );
 
-// sceltaUtente = true;
-
-// if (sceltaUtente !== "pari" || sceltaUtente !== "dispari") {
-//     sceltaUtente = false;
-//     console.log ("Devi inserire solo la parola 'pari' o 'dispari' ")
-// }
-
-// const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-
-// function generazioneNumeroRandom () {
-//     const numeroRandom = parseInt(Math.floor(Math.random() * 6));
-//     return numeroRandom;
-// }
-
-// const numeroComputer = generazioneNumeroRandom();
-// const sommaNumeri = numeroUtente + numeroComputer;
-
-function numeroPariDispari(numero) {
-    numero = false;
-    if(numero % 2 === 0) {
-        numero = true;
-    }
-    return numero;
+if (sceltaUtente === "pari" || sceltaUtente === "dispari") {
+    console.log("L'utente ha scelto: ", sceltaUtente)
+    sceltaUtente = true;
+}
+else {
+    console.log ("Devi inserire solo la parola 'pari' o 'dispari' ")
 }
 
-let ciao = numeroPariDispari(3)
-console.log(ciao)
+if(sceltaUtente === true) {
+    const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
+    function generazioneNumeroRandom () {
+        const numeroRandom = parseInt(Math.floor(Math.random() * 6));
+        return numeroRandom;
+    }
+
+    const numeroComputer = generazioneNumeroRandom();
+    const sommaNumeri = numeroUtente + numeroComputer;
+
+    function numeroPariDispari(numero) {
+        check = false;
+        if(numero % 2 === 0) {
+            check = true;
+        }
+        return check;
+    }
+
+    const controlloSommaNumeri = numeroPariDispari(sommaNumeri);
+
+    console.log("Il numero del giocatore è:", numeroUtente)
+    console.log("Il numero del Computer è:", numeroComputer)
+    
+
+    if ((sceltaUtente === "pari" && controlloSommaNumeri === true) || (sceltaUtente === "dispari" && controlloSommaNumeri === false)) {
+        console.log("Il giocatore ha vinto!")
+    }
+    else {
+        console.log("Il computer ha vinto!")
+    }
+}
