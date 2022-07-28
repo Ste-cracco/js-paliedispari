374a0eb62f4f1af6cf1813bd30bfcd1e9edc4811
Ste-cracco <s.cracco@yahoo.it>
2022-07-28 18:00:05 +0200

    Esercizio Pari/Dispari completato

--- JS/pari_dispari.js
+++ JS/pari_dispari.js
@@ -1,42 +1,38 @@
 let sceltaUtente = prompt("Scegli tra: 'pari' o 'dispari'" );
 
-if (sceltaUtente === "pari" || sceltaUtente === "dispari") {
-    console.log("L'utente ha scelto: ", sceltaUtente)
-    sceltaUtente = true;
+while (sceltaUtente !== "pari" && sceltaUtente !== "dispari") {
+    sceltaUtente = prompt("Scelta non corretta. Scegli tra: 'pari' o 'dispari'" );
+}
+
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+function generazioneNumeroRandom () {
+    const numeroRandom = parseInt(Math.floor(Math.random() * 6));
+    return numeroRandom;
+}
+
+const numeroComputer = generazioneNumeroRandom();
+const sommaNumeri = numeroUtente + numeroComputer;
+
+function numeroPariDispari(numero) {
+    check = false;
+    if(numero % 2 === 0) {
+        check = true;
+    }
+    return check;
+}
+
+const controlloSommaNumeri = numeroPariDispari(sommaNumeri);
+
+console.log("La scelta dell'Giocatore è:", sceltaUtente)
+console.log("Il numero del giocatore è:", numeroUtente)
+console.log("Il numero del Computer è:", numeroComputer)
+
+
+if ((sceltaUtente === "pari" && controlloSommaNumeri === true) || (sceltaUtente === "dispari" && controlloSommaNumeri === false)) {
+    console.log("Il Giocatore ha vinto!")
 }
 else {
-    console.log ("Devi inserire solo la parola 'pari' o 'dispari' ")
+    console.log("Il Computer ha vinto!")
 }
 
-if(sceltaUtente === true) {
-    const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-
-    function generazioneNumeroRandom () {
-        const numeroRandom = parseInt(Math.floor(Math.random() * 6));
-        return numeroRandom;
-    }
-
-    const numeroComputer = generazioneNumeroRandom();
-    const sommaNumeri = numeroUtente + numeroComputer;
-
-    function numeroPariDispari(numero) {
-        check = false;
-        if(numero % 2 === 0) {
-            check = true;
-        }
-        return check;
-    }
-
-    const controlloSommaNumeri = numeroPariDispari(sommaNumeri);
-
-    console.log("Il numero del giocatore è:", numeroUtente)
-    console.log("Il numero del Computer è:", numeroComputer)
-    
-
-    if ((sceltaUtente === "pari" && controlloSommaNumeri === true) || (sceltaUtente === "dispari" && controlloSommaNumeri === false)) {
-        console.log("Il giocatore ha vinto!")
-    }
-    else {
-        console.log("Il computer ha vinto!")
-    }
-}
